e3d9e4077471585c12d8523e3e8d40d82da0d58a
atiqulislamwb <atiqul@lintechgroup.co.uk>
2024-06-04 17:06:46 +0600

    first commit

---
+++ AirbnbApp/AirbnbAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbAppApp.swift
+//  AirbnbApp
+//
+//  Created by Atiqul Islam on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
